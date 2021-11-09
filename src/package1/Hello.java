87c3419fd32c0708849e22e09df9f5474924db8b
EmsCode82 <emsgithub@gmail.com>
2021-11-09 15:03:25 -0600

    .

---
+++ src/package1/Hello.java
@@ -0,0 +1,13 @@
+package package1;
+public class Hello {
+
+	public static void main(String[] args) {
+		int x = 6, y =6;
+		
+		if(x>=y) {
+			System.out.println("true");
+		}else {
+			System.out.println("false");
+		}
+	}
+}
